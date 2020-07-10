0aee0cd95d3f968d4548fce17eec3acffe4d13bb
Aleksandra Bułka <aleksandra.bulka@codility.com>
2020-07-10 14:06:05 +0200

    change concept to more manual parsing

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,74 +2,224 @@
 
 module Lib where
 
-import MyJSONData
-import GHC.Generics
-
+import Data.Maybe
 import Data.Text
-import Data.Aeson
+import Data.String
+
 import Pipes
 import Pipes.Parse
-import qualified Pipes.Aeson as PA
-import qualified Pipes.Prelude as P
-import qualified Data.Vector as V
-import qualified Data.ByteString as BS
+import Pipes.Lift
+
 import Control.Monad
-import qualified Data.HashMap.Strict as HM
-
-import Data.JsonStream.Parser
-import qualified Data.ByteString.Char8 as B
-import qualified Data.ByteString.Lazy.Char8 as BL
-import qualified Data.Aeson.Types as AT
-
-import System.Random
+import Control.Monad.State.Strict
+import Control.Applicative
 
 import qualified System.IO as IO
+import qualified Data.Text.IO as TIO
 
-test2 =  do
-  Just ma <- evalStateT PA.decode (do mapM_ yield [" { \"age\":   234, \"name\": \"",  "Steve\" } "] )
-  let _ = ma :: Either PA.DecodingError MyJSONData
-  case ma of
-    Left e   -> error $ "parse error: " ++ show e
-    Right a  -> putStrLn $ "got a Person: " ++ show a
+import qualified Pipes.Attoparsec as PAP
+import qualified Data.Attoparsec.Text as AP
+
+data OperationType = Modify String String | Add String String [String] | Remove String [String] 
+
+data TokenData = JString String | JNumber Double | JOpenBracket  | JCloseBracket  | JComma | JTrue | JFalse | JNull | JOpenSqBracket | JCloseSqBracket | JColon
+
+instance Show TokenData where
+  show (JString s) = show s
+  show (JNumber num) = show num
+  show JOpenBracket = show '{'
+  show JCloseBracket = show "}"
+  show JComma = show ","
+  show JColon = show ":"
+  show JOpenSqBracket = show "["
+  show JCloseSqBracket = show "]"
+  show JTrue = show "true"
+  show JFalse = show "false"
+  show JNull = show "null"
+
+write :: TokenData -> [Char]
+write (JString s) = show s
+write (JNumber num) = show num
+write JOpenBracket = ['{']
+write JCloseBracket = ['}']
+write JComma = [',']
+write JColon = [':']
+write JOpenSqBracket = ['[']
+write JCloseSqBracket = [']']
+write JTrue = "true"
+write JFalse = "false"
+write JNull = "null"
 
 
---The definition of Object in Data.Aeson is HashMap Text Value - 
---(you can think of them as glorified list of tuples; they even have list functions [2]).
--- read a line-oriented JSON file into object, counting parsing errors
+openBracket :: AP.Parser TokenData
+openBracket = const JOpenBracket <$> (AP.satisfy $ AP.inClass "{")
 
-valueParser :: Pipe B.ByteString (Value) IO ()
-valueParser = do_parse parse_output
-  where parse_output :: (ParseOutput Value)
-        parse_output = runParser (value)
-        do_parse :: ParseOutput Value -> Pipe B.ByteString (Value) IO ()
-        do_parse output = case output of
-            ParseYield value new_output -> do
-              lift $ putStrLn "emitting new json value"
-              lift $ print value
-              yield value
-              do_parse new_output
-            ParseNeedData cont -> do
-              lift $ putStrLn "in need of new data"
-              input <- await
-              lift $ putStrLn $ "injecting " ++ (B.unpack input) ++ " into the parser"
-              do_parse $ cont input
-            ParseDone remaining -> do
-              lift $ putStrLn "parsing done"
-              return ()
-            ParseFailed err -> lift $ putStrLn $ "PARSING ERROR: " ++ err
+closeBracket :: AP.Parser TokenData
+closeBracket = const JCloseBracket <$> (AP.satisfy $ AP.inClass "}")
 
-stdinProducer :: IO.Handle -> Producer B.ByteString IO ()
+comma :: AP.Parser TokenData
+comma = const JComma <$> (AP.satisfy $ AP.inClass ",")
+
+colon :: AP.Parser TokenData
+colon = const JColon <$> (AP.satisfy $ AP.inClass ":")
+
+openSqBracket :: AP.Parser TokenData
+openSqBracket = const JOpenSqBracket <$> (AP.satisfy $ AP.inClass "[")
+
+closeSqBracket :: AP.Parser TokenData
+closeSqBracket = const JCloseSqBracket <$> (AP.satisfy $ AP.inClass "]")
+
+nullParser :: AP.Parser TokenData
+nullParser = AP.string "null" *> return JNull
+
+boolParser :: AP.Parser TokenData
+boolParser = (trueParser *> return JTrue) <|> (falseParser *> return JFalse)
+  where
+    trueParser = AP.string "true" 
+    falseParser = AP.string "false" 
+
+doubleParser ::  AP.Parser TokenData
+doubleParser = fmap JNumber AP.double
+
+stringParser ::  AP.Parser TokenData
+stringParser = do
+    AP.string "\""
+    s <- AP.takeWhile (/= '\"' )
+    AP.string "\""
+    return $ JString (unpack s)
+
+parseJsonData :: AP.Parser TokenData
+parseJsonData = AP.skipSpace *> AP.choice [openBracket, closeBracket, comma, colon, openSqBracket, closeSqBracket, boolParser, doubleParser, stringParser, nullParser ]
+
+
+data JStack = JArray | JObject (Maybe String) 
+ 
+
+prefix :: [String] -> [String] -> Bool
+prefix [] _ = True
+prefix _ [] = False
+prefix (x:xs) (y:ys) = x == y && prefix xs ys
+
+calcPath :: [JStack] -> [String]
+calcPath = Prelude.reverse . mapMaybe fun  
+  where 
+      fun JArray = Nothing
+      fun (JObject s) = s
+
+valueChanger :: String -> String -> TokenData -> [JStack] -> Bool -> TokenData
+valueChanger k v curr_val (x:xs) is_key = case x of
+                                               (JObject (Just key)) -> do
+                                                    case curr_val of 
+                                                        (JString s) -> if ((not is_key) && key == k ) then (JString v) else curr_val
+                                                        JTrue -> if key == k then (JString v) else curr_val
+                                                        JFalse -> if key == k then (JString v) else curr_val
+                                                        JNull -> if key == k then (JString v) else curr_val
+                                                        (JNumber n) -> if key == k then (JString v) else curr_val
+                                                        _ ->  curr_val
+                                               _ ->  curr_val
+
+
+valueAdderToVal :: [String] -> TokenData -> [JStack] -> Bool -> Bool
+valueAdderToVal desired_path curr_val curr_stack is_key = case curr_val of
+                                                (JString s) -> if (calcPath curr_stack) == desired_path  && (not is_key) then True else False
+                                                JTrue -> if (calcPath curr_stack) == desired_path then True else False
+                                                JFalse -> if (calcPath curr_stack) == desired_path then True else False
+                                                JNull -> if (calcPath curr_stack) == desired_path then True else False
+                                                (JNumber n) ->if (calcPath curr_stack) == desired_path then True else False
+                                                _ -> False
+
+valueAdderToObj :: [String] -> TokenData -> [JStack] -> Bool -> Bool
+valueAdderToObj desired_path curr_val curr_stack is_key = case curr_val of
+                                                JOpenBracket -> if (calcPath curr_stack) == desired_path then True else False
+                                                _ -> False
+
+
+valueAdder :: String -> String -> [String] -> TokenData -> [JStack] -> Bool -> [TokenData]
+valueAdder k v desired_path curr_val curr_stack is_key = if (valueAdderToVal desired_path curr_val curr_stack is_key) then [JOpenBracket, (JString k), JColon, (JString v), JCloseBracket] else 
+                                                         if (valueAdderToObj desired_path curr_val curr_stack is_key) then [JOpenBracket, (JString k), JColon, (JString v), JComma] else [curr_val]
+
+
+valueRemover :: String -> [String] -> TokenData -> [JStack] -> Bool -> [TokenData]
+valueRemover key desired_path curr_val curr_stack is_key = if (desired_path++[key]) `prefix` (calcPath curr_stack) then [] else [curr_val]
+
+modFst :: (a -> a) -> Pipe TokenData (TokenData) (StateT (a,b) IO) ()
+modFst f = modify (\(a,b) -> (f a, b))
+
+modSnd :: (b -> b) -> Pipe TokenData (TokenData) (StateT (a,b) IO) ()
+modSnd f =  modify (\(a,b) -> (a,f b))
+
+putFst :: a -> Pipe TokenData (TokenData) (StateT (a,b) IO) ()
+putFst x = modify  (\(a,b) -> (x, b))
+
+putSnd ::  b -> Pipe TokenData (TokenData) (StateT (a,b) IO) ()
+putSnd x = modify  (\(a,b) -> (a, x))
+
+
+
+
+valueModifier :: OperationType -> Pipe TokenData (TokenData) (StateT ([JStack],Maybe TokenData) IO) a
+valueModifier operation = do 
+    val <- await
+    isKey <- case val of
+                JOpenBracket -> modFst (JObject Nothing :) >> return False
+                (JString s) -> do
+                    (x:xs) <- fst <$> get 
+                    case x of 
+                        (JObject Nothing) -> putFst (JObject (Just s):xs)  >> return True
+                        _ -> return False
+                JComma -> do
+                    (x:xs) <- fst <$> get
+                    case x of 
+                        (JObject s) -> do putFst (JObject Nothing:xs)  >> return False
+                        _ -> return False
+                JCloseBracket -> modFst Prelude.tail  >> return False
+                JOpenSqBracket -> modFst (JArray :)  >> return False
+                JCloseSqBracket ->  modFst Prelude.tail  >> return False
+                _ -> return False
+    curr_stack <- fst <$> get 
+    case curr_stack of
+        (x:xs) -> do
+            case operation of 
+                (Modify k v) -> yield $ valueChanger k v val curr_stack isKey
+                (Add k v path) -> mapM_ yield (valueAdder k v path val curr_stack isKey)
+                (Remove k path) -> mapM_ yield (valueRemover k path val curr_stack isKey)
+        _ -> yield val
+  
+    valueModifier operation
+    
+
+stdinProducer :: (MonadIO m) => IO.Handle -> Producer Text m ()
 stdinProducer h = do
-  eof <- lift $ IO.hIsEOF h
+  eof <- liftIO $ IO.hIsEOF h
   unless eof $ do
-    str <- lift (B.hGet h 100)
+    str <- liftIO (TIO.hGetChunk h)
     yield str
     stdinProducer h
 
-bsWriter :: Consumer Value IO ()
-bsWriter = do
-  lift $ B.putStrLn "Writer"
+parsingProducer ::(MonadIO m) =>  IO.Handle -> Producer TokenData m (Either (PAP.ParsingError, Producer Text m ()) ())
+parsingProducer h = PAP.parsed parseJsonData (stdinProducer h)
+
+waiter :: Pipe TokenData (TokenData) (StateT ([JStack], Maybe TokenData) IO)  a
+waiter = do
+    val_new <- await
+    val_old <- snd <$> get 
+    case val_old of
+        Nothing -> do
+            putSnd (Just val_new)
+        Just JComma -> case val_new of
+            JComma ->  putSnd (Just val_new)
+            JCloseBracket -> putSnd (Just val_new)
+            x -> yield JComma >> putSnd (Just val_new)
+        Just JOpenBracket -> case val_new of
+            JComma ->  putSnd (Just JOpenBracket)
+            x -> yield JOpenBracket >> putSnd (Just val_new)
+        Just x -> yield x >> putSnd (Just val_new)
+    waiter
+
+
+bsWriter :: (MonadIO m) => IO.Handle  -> Consumer (TokenData) m a
+bsWriter h = do
   val <- await
-  lift $ print val
-  bsWriter
+  liftIO $ IO.hPutStr h $ write val
+  bsWriter h
 
--- src/Main.hs
+++ src/Main.hs
@@ -2,22 +2,59 @@ module Main where
 
 import System.Environment
 import Lib
-import Control.Monad (unless)
-import qualified Pipes.ByteString as P
 
+import Data.Text 
+import Data.Maybe
+import Text.Read
+import Control.Monad.State.Strict
 import Pipes
+import Pipes.Attoparsec
 import System.IO
 
 
-main = withFile "testing.json"  ReadMode  $ \hIn  ->
-       withFile "outFile.txt" WriteMode $ \hOut ->
-       runEffect $ stdinProducer hIn >-> valueParser >-> bsWriter
+getPath :: IO [String]
+getPath = do
+    input <- getLine
+    case parseInput input of 
+        Nothing -> return []
+        Just path_part -> do
+            more_path <- getPath
+            return (path_part : more_path)
 
--- main =do
---   runEffect $ stdinProducer2 >-> jsonArrayGrouper >-> randomSampler >-> bsWriter 
+parseInput :: String -> Maybe String
+parseInput input = if input == ":q" then Nothing else (readMaybe input):: Maybe String
 
-  -- getArgs >>= mapM_ (\file -> readObjectFromFileIntoJ file);
-  --   --test2;
-  --   putStrLn ("Press any key to exit");
-  --   line <- getLine;
-  --   putStrLn ("Exiting");
+
+main = do
+  putStrLn "Please input file name"  
+  inFile <- getLine
+  withFile inFile  ReadMode  $ \hIn  ->
+    withFile "outFile.txt" WriteMode $ \hOut -> do
+    putStrLn "Please select what would you like to do: ADD, REMOVE, MODIFY"  
+    option <- getLine
+    case option of 
+      "MODIFY" -> do 
+        putStrLn "Enter key to be modified"  
+        key <- getLine
+        putStrLn "Enter value for this key"  
+        val <- getLine
+        runStateT (runEffect $ parsingProducer hIn  >-> valueModifier (Modify key val) >-> bsWriter hOut) ([], Nothing)
+        --either brac z lewej strony jak blad
+      "ADD" -> do 
+        putStrLn "Enter key to be added"  
+        key <- getLine
+        putStrLn "Enter value for this key"  
+        val <- getLine
+        putStrLn "Enter path where to insert this key, enter :q to stop"  
+        path <- getPath
+        runStateT (runEffect $ parsingProducer hIn  >-> valueModifier (Add key val path)>-> bsWriter hOut) ([], Nothing)
+      "REMOVE" -> do 
+        putStrLn "Enter key to be removed"  
+        key <- getLine
+        putStrLn "Enter path where to insert this key, enter :q to stop"  
+        path <- getPath
+        x <- runStateT (runEffect $ parsingProducer hIn  >-> valueModifier (Remove key path) >-> waiter >-> bsWriter hOut) ([], Nothing)
+        hPutStr hOut "}"
+        return x
+      _ -> putStrLn "Incorrect option" >> runStateT (runEffect $ parsingProducer hIn  >-> bsWriter hOut) ([], Nothing)
+
